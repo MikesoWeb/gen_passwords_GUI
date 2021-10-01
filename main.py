3514fcac4e18f0a06438b7413eca5d815ae988d4
MikesoWeb <47977649+MikesoWeb@users.noreply.github.com>
2021-10-01 16:21:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
+import string
 from tkinter import *
-from tkinter import messagebox
- 
- 
 
+chars = string.ascii_letters + string.ascii_lowercase + string.ascii_uppercase + string.punctuation
 
-def generate():
-  pass  
- 
 root = Tk()
-root.title("Генератор паролей 2020")
-root.geometry("450x150+150+100")
+root.title('Генератор пароля')
+root.geometry('550x170')
 
- 
-name_label = Label(text="Длинна пароля:")
+
+def gen_pass():
+    import random
+    password = ''
+    entry2.delete(0, END)
+
+    for j in range(int(get_number())):
+        password += random.choice(chars)
+    print(password)
+    return password
+
+
+def insert_data():
+    entry2.delete(0, END)
+    entry2.insert(0, gen_pass())
+
+    return entry2
+
+
+def get_number():
+    ent = entry.get()
+    if not ent:
+        ent = 24
+        entry.insert(INSERT, ent)
+    return ent
+
+
+name_label = Label(text='Длина', font="Courier 12 italic")
 name_label.grid(row=0, column=0, padx=10, pady=20, ipadx=10, sticky="w")
-name_label = Label(text="Ваш пароль:")
-name_label.grid(row=1, column=0, padx=10, pady=0, ipadx=10,sticky="w")
 
- 
- 
-name_entry = Entry()
-name_entry1 = Entry()
+name_label2 = Label(text='Пароль', font="Courier 12 italic")
+name_label2.grid(row=1, column=0, padx=10, pady=20, ipadx=10, sticky="w")
 
-name_entry.grid(row=0,column=1, padx=5, pady=5)
-name_entry1.grid(row=1,column=1, padx=5, pady=5)
-name_entry.place(x=430, y=30, height=30, width=130, anchor="e")
-name_entry1.place(x=430, y=70, height=30, width=300, anchor="e")
+entry = Entry()
+entry.grid(row=1, column=1, padx=5, pady=5)
+entry.place(x=500, y=30, height=20, width=400, anchor="e")
 
- 
-# вставка начальных данных
-name_entry.insert(0,"")
-name_entry1.insert(0, "Здесь будет пароль")
+entry2 = Entry()
+entry2.grid(row=1, column=1, padx=10, pady=10)
+entry2.place(x=500, y=100, height=20, width=400, anchor="e")
 
+button = Button(text='Отправить', command=insert_data, font="Courier 12 italic")
 
- 
-
-gen_button = Button(text="Сгенерировать", command=generate)
- 
-
-
-gen_button.grid(row=3, column=1, padx=5, pady=5, sticky="e")
- 
+button.grid()
+button.place(x=400, y=120)
 root.mainloop()
